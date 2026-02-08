c70094fa671402b608eb3124128cbf4e32415c48
jordiP28 <jorda2008pacheco@hotmail.com>
2026-02-07 20:46:42 -0500

    operaciones matematicas

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ num1 = int(input())
 num2 = int(input()) 
 
 suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
 print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
