ac66af3cab97112e261dbe418696a4ece3a5fec5
jordiP28 <jorda2008pacheco@hotmail.com>
2026-02-07 20:51:19 -0500

    agg modulo

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ suma = num1 + num2
 resta = num1 - num2
 multiplicacion = num1 * num2
 division = num1 / num2
+modulo = num1 % num2
 
 print(suma)
 print(resta)
 print(multiplicacion)
 print(division)
+print(modulo)
